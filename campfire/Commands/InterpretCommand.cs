6a9f96bf2842a9c0a705570e98ea66ba6dc4cad9
Adam Blahuta <adam.blahuta@gmail.com>
2023-07-16 21:07:15 -0400

    Added Step Timing

--- campfire/Commands/InterpretCommand.cs
+++ campfire/Commands/InterpretCommand.cs
@@ -7,11 +7,16 @@ public static class InterpretCommand
 {
     public static void ExecuteCommand(string[] args, ref MessageHandler.ExitCodes exitCode, bool printToConsole)
     {
+
+        var previousMs = GetTime();
         var tokens = LexCommand.ExecuteWork(args, ref exitCode);
+        Console.WriteLine($"[Lexer took {GetTime() - previousMs} ms]");
 
         if (exitCode == MessageHandler.ExitCodes.Successful)
         {
+            previousMs = GetTime();
             var statements = ParseCommand.ExecuteWork(tokens, ref exitCode);
+            Console.WriteLine($"[Parser took {GetTime() - previousMs} ms]");
 
             if (ErrorHandler.HadError)
             {
@@ -22,15 +27,25 @@ public static class InterpretCommand
             var interpreter = new TreeWalkInterpreter.TreeWalkInterpreter();
 
             Resolver resolver = new Resolver(interpreter);
+
+            previousMs = GetTime();
             resolver.Resolve(statements);
+            Console.WriteLine($"[Resolver took {GetTime() - previousMs} ms]");
             
             if (ErrorHandler.HadError)
             {
                 Console.WriteLine("Interpreter skipped due to Resolving Error");
                 return;
             }
-            
+
+            previousMs = GetTime();
             interpreter.Interpret(statements);
+            Console.WriteLine($"[Interpreter took {GetTime() - previousMs} ms]");
         }    
+    }
+
+    private static long GetTime()
+    {
+        return DateTime.Now.Ticks / TimeSpan.TicksPerMillisecond;
     }
 }
